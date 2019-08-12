aed062494c7293985a05867ec33827a98a1bb9e8
xovel <xovel@vip.qq.com>
2019-08-12 16:59:55 +0800

    @z9i/date-format v0.0.2
    - using es5
    - using umd

--- packages/dateFormat/lib/index.js
+++ packages/dateFormat/lib/index.js
@@ -1,3 +1,9 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global.dateFormat = factory());
+}(this, function () {
+
 /**
  * 日期时间的格式化处理
  *
@@ -14,19 +20,22 @@
  * @param {number|string|Date} v 待解析
  * @param {string} format 解析后的格式，如果传入 `timestamp` 则直接返回时间戳
  */
-module.exports = function dateFormat(v, format = 'YYYY-MM-DD') {
+return function dateFormat(v, format) {
   if (!v) {
     return v;
+  }
+  if (typeof format !== 'string') {
+    format = 'YYYY-MM-DD'
   }
   // 如果传入国际标准字符串，并且刚好是 12 点，则将上午标识修订为下午
   if (typeof v === 'string' && /12(:00(:00)?)? AM/i.test(v)) {
     v = v.replace(/AM/i, 'PM');
   }
-  let _date = v instanceof Date ? v : new Date(/^\d+$/.test(v) ? +v : v);
-  let _time = _date.getTime();
+  var _date = v instanceof Date ? v : new Date(/^\d+$/.test(v) ? +v : v);
+  var _time = _date.getTime();
 
   if (isNaN(_time) && typeof v === 'string') {
-    let match = v.match(/^(\d+)[-/](\d+)[-/](\d+)[Tt\s](\d+):(\d+):(\d+)/);
+    var match = v.match(/^(\d+)[-/](\d+)[-/](\d+)[Tt\s](\d+):(\d+):(\d+)/);
     if (match) {
       _date = new Date(+match[1], +match[2] - 1, +match[3], +match[4], +match[5], +match[6]);
       _time = _date.getTime();
@@ -41,9 +50,9 @@ module.exports = function dateFormat(v, format = 'YYYY-MM-DD') {
     return _time;
   }
 
-  let ret = format;
+  var ret = format;
 
-  const o = {
+  var o = {
     'M+': _date.getMonth() + 1, // month
     'd+': _date.getDate(), // day
     'D+': _date.getDate(), // day
@@ -63,7 +72,7 @@ module.exports = function dateFormat(v, format = 'YYYY-MM-DD') {
   });
 
   // 其他格式化处理
-  for (let k in o) {
+  for (var k in o) {
     ret = ret.replace(new RegExp(k, 'g'), function (a) {
       // 补零操作
       if (a.length > 1) {
@@ -76,3 +85,4 @@ module.exports = function dateFormat(v, format = 'YYYY-MM-DD') {
   return ret;
 
 }
+}));
