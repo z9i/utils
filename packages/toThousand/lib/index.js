c65448814fb8e9b577e8292b4a6f454b854e3087
xovel <xovel@vip.qq.com>
2019-08-12 17:22:41 +0800

    @z9i/to-thousand v0.0.2
    - using es5
    - using umd

--- packages/toThousand/lib/index.js
+++ packages/toThousand/lib/index.js
@@ -1,14 +1,22 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global.toThousand = factory());
+}(this, function () {
 /**
  * 数字转千分符
  * @param {String} v 待转字符串
  * @param {String} sep 分隔符
  */
-module.exports = function toThousand(v, sep = ',') {
+return function toThousand(v, sep) {
+  var sepChar = typeof sep === 'string' ? sep : ','
   if (Number(v)) {
     return String(v).trim().replace(/^(\d+)((\.\d+)?)$/, function (_, a, b) {
-      return a.replace(/(\d)(?=(?:\d{3})+$)/g, function (x) { return x + sep }) + b;
+      return a.replace(/(\d)(?=(?:\d{3})+$)/g, function (x) { return x + sepChar }) + b;
     });
   }
   // 数值换转失败则原值返回
   return v;
 }
+
+}));
